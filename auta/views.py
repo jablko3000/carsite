34bb610267c004ed92c6c612f89b3fd54965886e
jablko <jablko3000@gmail.com>
2023-11-06 19:04:37 +0100

    Basic reservation and detail pages

--- auta/views.py
+++ auta/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from datetime import datetime, date
 
 from .models import Auto
 
@@ -10,3 +11,16 @@ class HomePageView(generic.ListView):
     context_object_name = "auta_list"
     def get_queryset(self):
         return Auto.objects.all()
+    
+class DetailView(generic.DetailView):
+    model = Auto
+    template_name = "auta/detail.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['dnesni_datum'] = date.today()
+        context['aktualni_cas'] = datetime.now().time()
+        return context
+
+def reserve(request, auto_id):
+    auto = get_object_or_404(Auto, pk = auto_id)
+    return HttpResponse("Úspěšně rezervováno")
